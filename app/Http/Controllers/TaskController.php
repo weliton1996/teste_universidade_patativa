090a4c05e77b6d11203bcd8abc9945e3e81271e3
José Weliton Silva dos Santos <welitonkira2gmail.com>
2024-03-06 17:09:12 -0300

    fix:token_csrf

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -37,61 +37,62 @@ class TaskController extends Controller
 
     public function create(Request $request)
     {
-        $request->validate(
-            [
-                'name' => 'required|string|between:3,30',
-                'detals' => 'required|max:300',
-                'category' => 'required',
-                'user_id' => 'required',
-            ],
-            [
-                'name.required' => 'O campo nome é obrigatório.',
-                'name.string' => 'O campo nome deve ser uma string.',
-                'name.between' => 'O campo nome deve ter entre :min e :max caracteres.',
+        if ($request->input('_token') != '') {
+            $request->validate(
+                [
+                    'name' => 'required|string|between:3,30',
+                    'detals' => 'required|max:300',
+                    'category' => 'required',
+                    'user_id' => 'required',
+                ],
+                [
+                    'name.required' => 'O campo nome é obrigatório.',
+                    'name.string' => 'O campo nome deve ser uma string.',
+                    'name.between' => 'O campo nome deve ter entre :min e :max caracteres.',
 
-                'detals.required' => 'O campo detalhes é obrigatório.',
-                'detals.max' => 'O campo detalhes não pode ter mais de :max caracteres.',
+                    'detals.required' => 'O campo detalhes é obrigatório.',
+                    'detals.max' => 'O campo detalhes não pode ter mais de :max caracteres.',
 
-                'category.required' => 'O campo categoria é obrigatório.',
+                    'category.required' => 'O campo categoria é obrigatório.',
 
-                'user_id.required' => 'O campo ID do usuário é obrigatório.',
-            ]
-        );
+                    'user_id.required' => 'O campo ID do usuário é obrigatório.',
+                ]
+            );
 
-        $categoryName = $request->input('category');
+            $categoryName = $request->input('category');
 
-        // Verificar se a categoria já existe
-        $existingCategory = Category::where('name', $categoryName)->first();
+            // Verificar se a categoria já existe
+            $existingCategory = Category::where('name', $categoryName)->first();
 
-        $category_id = null;
+            $category_id = null;
 
-        if (!$existingCategory && $categoryName) {
-            // Se não existir, então inserir
-            $category = new Category();
-            $category->name = $categoryName;
-            $category->save();
+            if (!$existingCategory && $categoryName) {
+                // Se não existir, então inserir
+                $category = new Category();
+                $category->name = $categoryName;
+                $category->save();
 
-            $category_id = $category->id;
+                $category_id = $category->id;
 
-            // Restante do seu código para criar a tarefa
-        } else {
-            $category = new Category();
+                // Restante do seu código para criar a tarefa
+            } else {
+                $category = new Category();
 
-            $category_id = $existingCategory->id;
+                $category_id = $existingCategory->id;
+            }
+
+
+            $task = [
+                'name' => $request->input('name'),
+                'detals' => $request->input('detals'),
+                'category_id' => $category_id,
+                'user_id' => $request->input('user_id'),
+            ];
+
+            // dd($task);
+
+            Task::create($task);
         }
-
-        
-        $task = [
-            'name' => $request->input('name'),
-            'detals' => $request->input('detals'),
-            'category_id' => $category_id,
-            'user_id' => $request->input('user_id'),
-        ];
-
-        // dd($task);
-
-        Task::create($task);
-
         // dd($tasks);
 
         return redirect()->route('mytasks');
@@ -117,80 +118,82 @@ class TaskController extends Controller
     public function update(Request $request)
     {
 
-        $request->validate(
-            [
-                'name' => 'required|string|between:3,30',
-                'detals' => 'required|max:300',
-                'category' => 'required',
-                'user_id' => 'required',
-            ],
-            [
-                'name.required' => 'O campo nome é obrigatório.',
-                'name.string' => 'O campo nome deve ser uma string.',
-                'name.between' => 'O campo nome deve ter entre :min e :max caracteres.',
+        if ($request->input('_token') != '') {
+            $request->validate(
+                [
+                    'name' => 'required|string|between:3,30',
+                    'detals' => 'required|max:300',
+                    'category' => 'required',
+                    'user_id' => 'required',
+                ],
+                [
+                    'name.required' => 'O campo nome é obrigatório.',
+                    'name.string' => 'O campo nome deve ser uma string.',
+                    'name.between' => 'O campo nome deve ter entre :min e :max caracteres.',
 
-                'detals.required' => 'O campo detalhes é obrigatório.',
-                'detals.max' => 'O campo detalhes não pode ter mais de :max caracteres.',
+                    'detals.required' => 'O campo detalhes é obrigatório.',
+                    'detals.max' => 'O campo detalhes não pode ter mais de :max caracteres.',
 
-                'category.required' => 'O campo categoria é obrigatório.',
+                    'category.required' => 'O campo categoria é obrigatório.',
 
-                'user_id.required' => 'O campo ID do usuário é obrigatório.',
-            ]
-        );
-        // Descriptografar o ID
-        $task_id= $request->input('task_id');
+                    'user_id.required' => 'O campo ID do usuário é obrigatório.',
+                ]
+            );
+            // Descriptografar o ID
+            $task_id = $request->input('task_id');
 
-        // dd($task_id);
+            // dd($task_id);
 
-        $categoryName = $request->input('category');
+            $categoryName = $request->input('category');
 
-        // Verificar se a categoria já existe
-        $existingCategory = Category::where('name', $categoryName)->first();
+            // Verificar se a categoria já existe
+            $existingCategory = Category::where('name', $categoryName)->first();
 
-        $category_id = null;
+            $category_id = null;
 
-        if (!$existingCategory && $categoryName) {
-            // Se não existir, então inserir
-            $category = new Category();
-            $category->name = $categoryName;
-            $category->save();
+            if (!$existingCategory && $categoryName) {
+                // Se não existir, então inserir
+                $category = new Category();
+                $category->name = $categoryName;
+                $category->save();
 
-            $category_id = $category->id;
+                $category_id = $category->id;
 
-            // Restante do seu código para criar a tarefa
-        } else {
-            $category = new Category();
+                // Restante do seu código para criar a tarefa
+            } else {
+                $category = new Category();
 
-            $category_id = $existingCategory->id;
+                $category_id = $existingCategory->id;
+            }
+
+
+
+            $task = [
+                'name' => $request->input('name'),
+                'detals' => $request->input('detals'),
+                'category_id' => $category_id,
+                'user_id' => $request->input('user_id'),
+            ];
+
+            Task::where('id', $task_id)->update($task);
+
+            // dd($task);
         }
 
-        
-
-        $task = [
-            'name' => $request->input('name'),
-            'detals' => $request->input('detals'),
-            'category_id' => $category_id,
-            'user_id' => $request->input('user_id'),
-        ];
-
-        Task::where('id', $task_id)->update($task);
-
-        // dd($task);
-
-        
 
         return redirect()->route('mytasks');
     }
 
-    public function delete( Request $request){
+    public function delete(Request $request)
+    {
+        if ($request->input('_token') != '') {
+            $id = decrypt($request->input('task_id'));
 
-        $id = decrypt($request->input('task_id'));
-
-        $task= Task::find($id);
-    //    dd($task);
-        $task->delete();
+            $task = Task::find($id);
+            //    dd($task);
+            $task->delete();
+        }
 
         return redirect()->route('mytasks');
-        
     }
 }
